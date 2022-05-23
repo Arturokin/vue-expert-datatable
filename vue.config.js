3c65e0f62a878080be886581bd90ab309ab18f33
Arturo Vicencio <avicencio@oneconsultores.com>
2022-05-23 17:15:12 -0400

    Se extrae el css aparte

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 const mode = process.env.NODE_ENV || 'development'
 
 module.exports = {
+    css: {
+        extract: {
+            filename: 'vue_expert_datatable.css',
+            ignoreOrder: true
+        }
+    },
     configureWebpack: {
         resolve: {
             alias: {
